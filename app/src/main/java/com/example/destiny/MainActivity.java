b738b128a116ab38d410af6d629da7d85bf60aef
umang <iumangsharma001@gmail.com>
2020-05-11 14:25:12 +0530

    Layout complete

---
+++ app/src/main/java/com/example/destiny/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.destiny;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView story_textview;
+    Button answer1;
+    Button answer2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
